62d596540e207478715e7b6d6c4d06d3fa80b6f4
shalin <shalin.rono@student.moringaschool.com>
2021-08-21 23:40:52 +0300

    add id and class in class goals

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -3,6 +3,19 @@ export class Quote {
   constructor(public id: number=0,public name: string='',public description: string='',public author: string='',public completeDate: Date=(new Date)){
     this.showDescription=false;
   }
+}  
 
 
-}
+
+
+
+
+
+// export class Quote {
+//   showDescription: boolean;
+//   constructor(public id: number=0,public name: string='',public description: string='',public author: string='',public completeDate: Date=(new Date)){
+//     this.showDescription=false;
+//   }
+
+
+// }
